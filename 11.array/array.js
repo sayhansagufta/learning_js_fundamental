cf81f954e12ce666ab576dce2095e65bac24c30b
Shayhan Sagufta <sayhansagufta99@gmail.com>
2024-10-17 10:55:19 +0700

    menambahkan latihan javascript array

---
+++ 11.array/array.js
@@ -0,0 +1,37 @@
+const names = ["Naruto", "Sasuke", "Sakura"];
+
+// menambahkan objek baru array dengan nama Madara
+// names.push("Madara");
+
+// menghapus salah satu objek dengan method splice
+// names.splice(1, 2);
+
+// names.length
+
+// names[2] = "Obito";
+
+// names.push('Boruto')
+
+// names.reverse()
+
+// const grid = [
+//     [1, 2, 3], // 0
+//     [4, 5, 6], // 1
+//     [7, 8, 9], // 2
+// ]
+
+// console.log(grid[0][2]);
+
+// const training = ["Enigma", "Camp", "Code", "Node"];
+// let size = training.length;
+// console.log(size)
+
+// const buah = ["Pisang", "Jeruk", "Apel", "Mangga"];
+// buah.push("Anggur");
+// console.log(buah)
+
+const name1 = ["Cecilie", "Lone"];
+const name2 = ["Emil", "Tobias", "Linus"];
+const name3 = ["Robin", "Morgan"];
+const students = name1.concat(name2, name3);
+console.log(students);
